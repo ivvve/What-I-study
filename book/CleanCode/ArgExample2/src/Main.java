7a49882223d203ad553854ef9e0f4cb7420b0155
devson <sonyc5720@gmail.com>
2020-04-15 01:02:16 +0900

    cleancode: Args - 1차 초안

---
+++ book/CleanCode/ArgExample2/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplication(logging, port, directory);
+        } catch (ArgsException e) {
+            System.out.print("Argument error: %s\n", e.errorMessage());
+        }
+    }
+}
