de2b0bc046d4831c44ea362f2aafd8374da1af01
devson <sonyc5720@gmail.com>
2020-04-23 07:42:34 +0900

    cleancode: 16. SerialDate - refactor p.350
    - Month enum 생성

---
+++ book/CleanCode/code_examples/src/main/java/serialdate/Month.java
@@ -0,0 +1,33 @@
+package serialdate;
+
+public enum Month {
+    JANUARY(1),
+    FEBRUARY(2),
+    MARCH(3),
+    APRIL(4),
+    MAY(5),
+    JUNE(6),
+    JULY(7),
+    AUGUST(8),
+    SEPTEMBER(9),
+    OCTOBER(10),
+    NOVEMBER(11),
+    DECEMBER(12);
+
+    private final int index;
+
+    Month(int index) {
+        this.index = index;
+    }
+
+    public static Month make(int monthIndex) {
+        for (Month m : Month.values()) {
+
+            if (m.index == monthIndex) {
+                return m;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid month index " + monthIndex);
+    }
+}
