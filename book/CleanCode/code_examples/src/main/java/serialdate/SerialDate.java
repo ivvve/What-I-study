b6b83009ef7bad694916ae37b19b51424054cc62
devson <sonyc5720@gmail.com>
2020-04-22 22:33:24 +0900

    cleancode: 16. SerialDate - refactor p.347
    - javadoc pre로 감싼 형식으로 변경

--- book/CleanCode/code_examples/src/main/java/serialdate/SerialDate.java
+++ book/CleanCode/code_examples/src/main/java/serialdate/SerialDate.java
@@ -44,22 +44,23 @@ import java.util.*;
 import static serialdate.MonthConstants.*;
 
 /**
+ * <pre>
  * An abstract class that defines our requirements for manipulating dates,
  * without tying down a particular implementation.
- * <p/>
+ *
  * Requirement 1 : match at least what Excel does for dates;
  * Requirement 2 : class is immutable;
- * <p/>
+ *
  * Why not just use java.util.Date?  We will, when it makes sense. At times,
  * java.util.Date can be *too* precise - it represents an instant in time,
  * accurate to 1/1000th of a second (with the date itself depending on the
  * time-zone).  Sometimes we just want to represent a particular day (e.g. 21
  * January 2015) without concerning ourselves about the time of day, or the
  * time-zone, or anything else.  That's what we've defined SerialDate for.
- * <p/>
+ *
  * You can call getInstance() to get a concrete subclass of SerialDate,
  * without worrying about the exact implementation.
- *
+ * </pre>
  * @author David Gilbert
  */
 public abstract class SerialDate implements Comparable,
