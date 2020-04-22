5ab811324a4444d2903441c5f8ec0dbcae310f0d
devson <sonyc5720@gmail.com>
2020-04-23 08:57:11 +0900

    cleancode: 16. SerialDate - refactor p.354
    - 상수 옮김
    SpreadsheetDate에서만 사용되는 상수를 옮겼다.
    사실 일관성을 위해 DayDate에 private field로 감춰두고 사용하는게 맞지만 아무곳에서도 이걸 사용하지 않는다.
    새로운 DayDate 구현이 생겼을 때 다시 DayDate로 옮기는 건 어렵지 않기 때문에 SpreadsheetDate로 옮겼다.

--- book/CleanCode/code_examples/src/main/java/serialdate/SpreadsheetDate.java
+++ book/CleanCode/code_examples/src/main/java/serialdate/SpreadsheetDate.java
@@ -79,6 +79,13 @@ import java.util.Date;
  * @author David Gilbert
  */
 public class SpreadsheetDate extends DayDate {
+    static final int[] AGGREGATE_DAYS_TO_END_OF_PRECEDING_MONTH =
+            {0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+
+    static final int[]
+            LEAP_YEAR_AGGREGATE_DAYS_TO_END_OF_PRECEDING_MONTH =
+            {0, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366};
+
     /**
      * The lowest year value supported by this date format.
      */
@@ -441,7 +448,7 @@ public class SpreadsheetDate extends DayDate {
      */
     private int calcSerial(final int d, final int m, final int y) {
         final int yy = ((y - 1900) * 365) + DayDate.leapYearCount(y - 1);
-        int mm = DayDate.AGGREGATE_DAYS_TO_END_OF_PRECEDING_MONTH[m];
+        int mm = AGGREGATE_DAYS_TO_END_OF_PRECEDING_MONTH[m];
         if (m > Month.FEBRUARY.getIndex()) {
             if (DayDate.isLeapYear(y)) {
                 mm = mm + 1;
