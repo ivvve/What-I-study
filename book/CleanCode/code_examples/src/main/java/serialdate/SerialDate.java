ee4bb4ce2c1fb7f00055a57e5e075aeec44255d7
devson <sonyc5720@gmail.com>
2020-04-22 22:29:00 +0900

    cleancode: 16. SerialDate - refactor p.347
    - 변경 이력 제거

--- book/CleanCode/code_examples/src/main/java/serialdate/SerialDate.java
+++ book/CleanCode/code_examples/src/main/java/serialdate/SerialDate.java
@@ -35,27 +35,6 @@ package serialdate;
  * Contributor(s):   -;
  *
  * $Id: SerialDate.java,v 1.7 2005/11/03 09:25:17 mungady Exp $
- *
- * Changes (from 11-Oct-2001)
- * --------------------------
- * 11-Oct-2001 : Re-organised the class and moved it to new package
- *               com.jrefinery.date (DG);
- * 05-Nov-2001 : Added a getDescription() method, and eliminated NotableDate
- *               class (DG);
- * 12-Nov-2001 : IBD requires setDescription() method, now that NotableDate
- *               class is gone (DG);  Changed getPreviousDayOfWeek(),
- *               getFollowingDayOfWeek() and getNearestDayOfWeek() to correct
- *               bugs (DG);
- * 05-Dec-2001 : Fixed bug in SpreadsheetDate class (DG);
- * 29-May-2002 : Moved the month constants into a separate interface
- *               (MonthConstants) (DG);
- * 27-Aug-2002 : Fixed bug in addMonths() method, thanks to N???levka Petr (DG);
- * 03-Oct-2002 : Fixed errors reported by Checkstyle (DG);
- * 13-Mar-2003 : Implemented Serializable (DG);
- * 29-May-2003 : Fixed bug in addMonths method (DG);
- * 04-Sep-2003 : Implemented Comparable.  Updated the isInRange javadocs (DG);
- * 05-Jan-2005 : Fixed bug in addYears() method (1096282) (DG);
- *
  */
 
 import java.io.Serializable;
