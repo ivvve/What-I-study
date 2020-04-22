4b3b30831dcc7934c454cfc8b5bfd67ad3759eef
devson <sonyc5720@gmail.com>
2020-04-23 08:23:12 +0900

    cleancode: 16. SerialDate - refactor p.354
    - deleted unused constants

--- book/CleanCode/code_examples/src/main/java/serialdate/DayDate.java
+++ book/CleanCode/code_examples/src/main/java/serialdate/DayDate.java
@@ -84,14 +84,8 @@ public abstract class DayDate implements Comparable, Serializable {
     static final int[] LAST_DAY_OF_MONTH =
             {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    static final int[] AGGREGATE_DAYS_TO_END_OF_MONTH =
-            {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
-
     static final int[] AGGREGATE_DAYS_TO_END_OF_PRECEDING_MONTH =
             {0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
-
-    static final int[] LEAP_YEAR_AGGREGATE_DAYS_TO_END_OF_MONTH =
-            {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366};
 
     static final int[]
             LEAP_YEAR_AGGREGATE_DAYS_TO_END_OF_PRECEDING_MONTH =
