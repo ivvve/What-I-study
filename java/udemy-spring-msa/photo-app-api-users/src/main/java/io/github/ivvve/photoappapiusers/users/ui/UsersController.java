cff05347d4a088b9f117cf39b723db23c85ff89f
devson <sonyc5720@gmail.com>
2020-06-29 00:11:34 +0900

    spring-msa: init users api server

---
+++ java/udemy-spring-msa/photo-app-api-users/src/main/java/io/github/ivvve/photoappapiusers/users/ui/UsersController.java
@@ -0,0 +1,15 @@
+package io.github.ivvve.photoappapiusers.users.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "working";
+    }
+}
---
+++ java/udemy-spring-msa/photo-app-api-users/src/test/java/io/github/ivvve/photoappapiusers/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ivvve.photoappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
