12f822ecdb8cd4d491e09e615126bad9f2533550
devson <sonyc5720@gmail.com>
2020-06-28 23:37:40 +0900

    spring-msa: added eureka server

---
+++ java/udemy-spring-msa/photo-app-discovery-server/src/test/java/io/github/ivvve/photoappdiscoveryserver/PhotoAppDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ivvve.photoappdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
